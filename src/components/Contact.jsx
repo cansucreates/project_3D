df3e5d23b2db9561b656b2ad3e118c5b30f4ef93
Cansu Aysagdic <cansu.aysagdic@ogr.iu.edu.tr>
2023-10-22 20:07:21 +0300

    "contact fixed"

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,7 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -33,8 +34,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_3w0aokq',
+        'template_xr1tp85',
         {
           from_name: form.name,
           to_name: "Cansu Aysagdic",
@@ -42,7 +43,7 @@ const Contact = () => {
           to_email: "caysagdic@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'g-Z93I6qK2NSCMSj7'
       )
       .then(
         () => {
@@ -87,7 +88,7 @@ const Contact = () => {
               name='name'
               value={form.name}
               onChange={handleChange}
-              placeholder="What's your good name?"
+              placeholder="What's your name?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
